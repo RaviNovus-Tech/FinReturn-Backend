64360b28eda7a38e08dcc5ec23281cfd1dfe2b03
sohailexe <div.sohailfaiz@gmail.com>
2025-07-25 17:25:31 +0500

    roi history added

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,14 @@ import authRoutes from "./authRoutes.js";
 import packageRoutes from "./packageRoutes.js";
 import userRoutes from "./userRoutes.js";
 import subscriptionRoutes from "./subscriptionsRoutes.js";
+import roiHistoryRoutes from "./roiHistoryRoutes.js";
 
 // Use routes
 router.use("/auth", authRoutes);
 router.use("/packages", packageRoutes);
 router.use("/users", userRoutes);
 router.use("/subscriptions", subscriptionRoutes);
+router.use("/roi", roiHistoryRoutes);
+
 
 export default router;
